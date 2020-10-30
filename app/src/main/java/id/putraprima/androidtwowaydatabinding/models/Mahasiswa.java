16efc85bf54a4a83605abc98d29ae997d64b6a85
siubie <putraprima@gmail.com>
2020-10-30 07:32:13 +0700

    Layout

---
+++ app/src/main/java/id/putraprima/androidtwowaydatabinding/models/Mahasiswa.java
@@ -0,0 +1,20 @@
+package id.putraprima.androidtwowaydatabinding.models;
+
+public class Mahasiswa {
+    private String nama;
+
+    public Mahasiswa() {
+    }
+
+    public Mahasiswa(String nama) {
+        this.nama = nama;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+}
